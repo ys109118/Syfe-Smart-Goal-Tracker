513c920bc7a4e6056b6e2bb58c5be67a515ae225
Yash Sharma <ys109118@gmail.com>
2026-01-07 12:44:02 +0530

    fix: import missing getTotalContributions function

--- src/components/SummaryBanner.jsx
+++ src/components/SummaryBanner.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { getTotalContributionsInUSD } from '../utils/localStorage';
+import { getTotalContributions, getTotalContributionsInUSD } from '../utils/localStorage';
 
 function SummaryBanner({ goals, exchangeRate }) {
   const [, forceUpdate] = useState({});
